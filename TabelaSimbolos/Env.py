4d8cc5865c6e8b4a46911ea4bcfbe079af66ddff
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-12 20:50:56 -0300

    Update Env.py

--- TabelaSimbolos/Env.py
+++ TabelaSimbolos/Env.py
@@ -1,5 +1,5 @@
 import Lexer
-from Inter import Id
+#from Inter import Id
 
 # File: Env.py
 def create_env(n):
@@ -24,3 +24,30 @@ def create_env(n):
 
 '''from lexer import Token
 from inter import Id'''
+
+# Usage example
+if __name__ == "__main__":
+    # Creating environment instances
+    global_env = create_env(None)
+    local_env1 = create_env(global_env)
+    local_env2 = create_env(local_env1)
+
+    # Creating identifiers
+    id1 = Id("x", Lexer.Tag.ID)
+    id2 = Id("y", Lexer.Tag.ID)
+    id3 = Id("z", Lexer.Tag.ID)
+
+    # Putting identifiers in environments
+    global_env["put"](id1, "Value of x")
+    local_env1["put"](id2, "Value of y in local_env1")
+    local_env2["put"](id3, "Value of z in local_env2")
+
+    # Getting identifiers from environments
+    search_id = "x"
+    result1 = global_env["get"](search_id)
+    result2 = local_env1["get"](search_id)
+    result3 = local_env2["get"](search_id)
+
+    print("Result from global_env:", result1)
+    print("Result from local_env1:", result2)
+    print("Result from local_env2:", result3)
