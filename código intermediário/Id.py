af520064d0bbbedec6729ff2c3f5ce7672aa4749
Weslley <106591789+Weslleyso@users.noreply.github.com>
2023-08-13 20:57:13 -0300

    Update Id.py

--- código intermediário/Id.py
+++ código intermediário/Id.py
@@ -1,8 +1,47 @@
-from lexer import Word
-from symbols import Type
-from Expr import Expr  
+# Simulando a classe Word
+def create_word(value, token_type):
+    return {
+        'value': value,
+        'token_type': token_type
+    }
 
-class Id(Expr):
-    def __init__(self, id_token, p, b):
-        super().__init__(id_token, p)
-        self.offset = b
+# Simulando a classe Type (sem atributos)
+type_instance = {}
+
+# Função para simular o método emit
+def emit(code):
+    print(code)
+
+# Função para simular o método emitjumps
+def emitjumps(test, t, f):
+    if t != 0 and f != 0:
+        emit("if " + test + " goto L" + str(t))
+        emit("goto L" + str(f))
+    elif t != 0:
+        emit("if " + test + " goto L" + str(t))
+    elif f != 0:
+        emit("iffalse " + test + " goto L" + str(f))
+    else:
+        pass  # Nada, pois tanto t quanto f caem diretamente
+
+# Função que simula o comportamento da classe Id
+def create_id(id_value, token_type, p, b):
+    op = create_word(id_value, token_type)
+    type_ = p
+    offset = b
+
+    def gen():
+        return create_id(op['value'], reduce(expr1), reduce(expr2), p, b)
+
+    def reduce(expression):
+        return create_id(op['value'], reduce(expr1), reduce(expr2), p, b)
+
+    def jumping(t, f):
+        emitjumps(str(op['value']), t, f)
+
+    return {
+        'gen': gen,
+        'reduce': reduce,
+        'jumping': jumping
+    }
+
