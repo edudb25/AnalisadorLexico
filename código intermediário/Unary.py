46a67ae3238856363bfb25b9b571b94f0a843e7c
Weslley <106591789+Weslleyso@users.noreply.github.com>
2023-08-13 21:16:10 -0300

    Update Unary.py

--- código intermediário/Unary.py
+++ código intermediário/Unary.py
@@ -1,16 +1,32 @@
 from lexer import Token
 from symbols import Type
-from Op import Op 
-class Unary(Op):
-    def __init__(self, tok, x):
-        super().__init__(tok, None)
-        self.expr = x
-        self.type = Type.max(Type.Int, x.type)
-        if self.type is None:
-            self.error("type error")
 
-    def gen(self):
-        return Unary(self.op, self.expr.reduce())
+def create_unary(tok, x):
+    def unary():
+        nonlocal tok, x
+        expr = x
+        type = Type.max(Type.Int, expr["type"])
+        if type is None:
+            error("type error")
+        return {
+            "tok": tok,
+            "expr": expr,
+            "type": type,
+            "gen": gen,
+            "__str__": __str__
+        }
+    
+    def gen():
+        return create_unary(tok, expr["reduce"]())
+    
+    def __str__():
+        return str(tok) + " " + str(expr)
+    
+    return unary()
 
-    def __str__(self):
-        return str(self.op) + " " + str(self.expr)
+def error(s):
+    raise Error(s)  # SUBSTITUIR Error com a implementação real de erro
+
+def emit(s):
+    print("\t" + s)
+
