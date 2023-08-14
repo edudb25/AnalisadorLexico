5c7344085be2076ae41c9f02536a8359ef4b1d6f
Weslley <106591789+Weslleyso@users.noreply.github.com>
2023-08-13 21:13:42 -0300

    Update Arith.py

--- código intermediário/Arith.py
+++ código intermediário/Arith.py
@@ -1,41 +1,34 @@
+from lexer import Token
 from symbols import Type
-from Op import Op  # Certifique-se de importar a classe Op se não estiver incluída no arquivo
 
-def Arith(tok, x1, x2):
-    expr1 = x1
-    expr2 = x2
-    op = tok.value
-    type_ = Type.max(x1['type'], x2['type'])
+def create_arith(tok, x1, x2):
+    def arith():
+        nonlocal tok, x1, x2
+        expr1 = x1
+        expr2 = x2
+        type = Type.max(expr1["type"], expr2["type"])
+        if type is None:
+            error("type error")
+        return {
+            "tok": tok,
+            "expr1": expr1,
+            "expr2": expr2,
+            "type": type,
+            "gen": gen,
+            "__str__": __str__
+        }
     
-    if type_ is None:
-        error("type error")
-
     def gen():
-        return Arith(op, reduce(expr1), reduce(expr2))
-
+        return create_arith(tok, expr1["gen"](), expr2["gen"]())
+    
     def __str__():
-        return str(expr1) + " " + str(op) + " " + str(expr2)
+        return str(expr1) + " " + str(tok) + " " + str(expr2)
+    
+    return arith()
 
-    return {
-        'expr1': expr1,
-        'expr2': expr2,
-        'op': op,
-        'type': type_,
-        'gen': gen,
-        '__str__': __str__
-    }
+def error(s):
+    raise Error(s)  # Substitua Error com a implementação real de erro
 
-def reduce(expression):
-    # Implemente a lógica para reduzir a expressão
-    pass
+def emit(s):
+    print("\t" + s)
 
-def error(message):
-    print("Error:", message)
-
-# Exemplo de uso
-tok = {'value': '+'}
-x1 = {'type': Type(), 'reduce': lambda: 5}
-x2 = {'type': Type(), 'reduce': lambda: 3}
-arith_expr = Arith(tok, x1, x2)
-print(arith_expr['gen']())
-print(arith_expr['__str__']())
