74955be8c1e13416a421384ff017b2dfa4a80b17
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-20 23:25:00 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,12 +4,12 @@ from Parser import Parser
 
 def main():
     try:
-        lex = Lexer()
-        parse = Parser(lex)
-        parse.program()
-        sys.stdout.write('\n')
+        lex = Lexer()  # Criação de uma instância de Lexer para análise léxica.
+        parse = Parser(lex)  # Criação de uma instância de Parser, passando a instância de Lexer para análise sintática.
+        parse.program()  # Inicia a análise do programa.
+        sys.stdout.write('\n')  # Escreve uma quebra de linha na saída padrão.
     except Exception as e:
-        sys.stderr.write(f'Error: {str(e)}\n')
+        sys.stderr.write(f'Erro: {str(e)}\n')  # Trata e imprime quaisquer exceções na saída de erro padrão.
 
 if __name__ == "__main__":
-    main()
+    main()  # Chama a função principal quando o script é executado.
