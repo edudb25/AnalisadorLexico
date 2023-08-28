b40751c578140931a7629186202641e5f577ad96
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-28 03:07:45 -0300

    Update Or.py

--- Código de desvio (jumping code)/Or.py
+++ Código de desvio (jumping code)/Or.py
@@ -1,1 +1,15 @@
+# Arq Or.py
+from lexer import Token
+from symbols import Type
+from expr import Expr, Logical
+from emit import emit, emitlabel, newlabel
 
+def create_or(tok, x1, x2):
+    super(tok, x1, x2)
+
+def jumping_or(t, f):
+    label = t if t != 0 else newlabel()
+    expr1.jumping(label, 0)
+    expr2.jumping(t, f)
+    if t == 0:
+        emitlabel(label)
