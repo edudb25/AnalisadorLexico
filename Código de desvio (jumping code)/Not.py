25738b7719948ff68184b53d37b0e871e11e31ab
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-28 03:20:11 -0300

    Update Not.py

--- Código de desvio (jumping code)/Not.py
+++ Código de desvio (jumping code)/Not.py
@@ -1,1 +1,13 @@
+# Arq Not.py
+from lexer import Token
+from symbols import Type
+from expr import Expr, Logical
 
+def create_not(tok, x2):
+    super(tok, x2, x2)
+
+def jumping_not(t, f):
+    expr2.jumping(f, t)
+
+def to_string_not():
+    return op["toString"]() + " " + expr2["toString"]()
