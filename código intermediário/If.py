d04c9f5d245d0820825e7a4025e003e2a98d19ce
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-27 22:10:29 -0300

    Create If.py

---
+++ código intermediário/If.py
@@ -0,0 +1,13 @@
+# File If.py (Python module)
+from symbols import Type, newlabel
+from stmt import Stmt
+from expr import Expr
+
+def create_if_stmt(x, s):
+    if x["type"] != Type.BOOL:
+        x["error"]("boolean required in if")
+
+    label = newlabel()  # label for the code for stmt
+    jumping(x, 0, s["a"])  # fall through on true, goto a on false
+    emitlabel(label)
+    gen(s, label, s["a"])
