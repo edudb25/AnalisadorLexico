cf6f06a393c83660e78d38bb88429f853f8ec305
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-28 07:23:57 -0300

    Update And.py

--- Código de desvio (jumping code)/And.py
+++ Código de desvio (jumping code)/And.py
@@ -1,8 +1,7 @@
 # Arq And.py
-from lexer import Token
-from symbols import Type
-from expr import Expr, Logical
-from emit import emit, emitlabel, newlabel
+import Token
+import Type
+import Expr, Logical
 
 def create_and(tok, x1, x2):
     super(tok, x1, x2)
