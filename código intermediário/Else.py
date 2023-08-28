f4c97cdb83fc79b33b0cc2c986bbff2c9670e5a9
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-27 22:23:38 -0300

    Create Else.py

---
+++ código intermediário/Else.py
@@ -0,0 +1,19 @@
+#Arquico Else.py
+import Type
+import newlabel
+from Stmt import stmt
+from Expr import expr
+from Emit import emit
+
+def create_else_stmt(x, s1, s2):
+    if x["type"] != Type.BOOL:
+        x["error"]("boolean required in if")
+
+    label1 = newlabel()  # label1 para stmt1
+    label2 = newlabel()  # label2 para stmt2
+    jumping(x, 0, label2)  # fall through to stmt1 on true
+    emitlabel(label1)
+    gen(s1, label1, s2["a"])
+    emit("goto L" + str(s2["a"]))
+    emitlabel(label2)
+    gen(s2, label2, s2["a"])
