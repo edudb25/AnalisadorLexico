c3f2921c2be38fbecdbc4cbc17fbe479b3779603
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-27 22:32:23 -0300

    Create While.py

---
+++ código intermediário/While.py
@@ -0,0 +1,25 @@
+# Arquivo While.py (Módulo Python)
+from symbols import Type, newlabel
+from stmt import Stmt
+from expr import Expr
+from emit import emit
+
+expr = None
+stmt = None
+
+def init_while(x, s):
+    global expr, stmt
+    expr = x
+    stmt = s
+
+    if x["type"] != Type.BOOL:
+        x["error"]("booleano necessário no while")
+
+def gen_while(b, a):
+    global expr, stmt
+    after = a  # Salvar rótulo a
+    jumping(expr, 0, a)
+    label = newlabel()  # Rótulo para stmt
+    emitlabel(label)
+    gen(stmt, label, b)
+    emit("goto L" + str(b))
