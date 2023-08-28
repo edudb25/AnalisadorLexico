8de39253017d9b719d3b5faca28f73178687b668
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-28 02:19:05 -0300

    Create Set.py

---
+++ código intermediário/Set.py
@@ -0,0 +1,24 @@
+# Arq Set.py
+from lexer import Id
+from symbols import Type
+from stmt import Stmt
+from expr import Expr
+from emit import emit, error
+
+def create_set_stmt(i, x):
+    id = i
+    expr = x
+
+    if check(id["type"], expr["type"]) is None:
+        error("erro de tipo")
+
+def check(p1, p2):
+    if Type.numeric(p1) and Type.numeric(p2):
+        return p2
+    elif p1 == Type.BOOL and p2 == Type.BOOL:
+        return p2
+    else:
+        return None
+
+def gen_set(b, a):
+    emit(id["toString"]() + " = " + expr["gen"]()["toString"]())
