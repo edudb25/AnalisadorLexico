4f03c7896f12ab5cd0d3ef2c324d80465bdc3e82
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-08-27 22:47:54 -0300

    Create Stmt.py

---
+++ código intermediário/Stmt.py
@@ -0,0 +1,9 @@
+# Arq Stmt.py
+from Node import node
+
+def gen_stmt(b, a):
+    pass  # Chamado com rótulos begin e after
+
+after = 0  # Salva o rótulo after
+
+enclosing = None  # Usado para instruções break
