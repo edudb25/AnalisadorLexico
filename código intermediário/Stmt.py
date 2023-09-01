5087ad7befd231946eb5246042fcb7706125660f
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-09-01 20:23:08 -0300

    Delete código intermediário directory

--- código intermediário/Stmt.py
+++
@@ -1,9 +0,0 @@
-# Arq Stmt.py
-import Node
-
-def gen_stmt(b, a):
-    pass  # Chamado com rótulos begin e after
-
-after = 0  # Salva o rótulo after
-
-enclosing = None  # Usado para instruções break
